79cc307853950fd068a2dd1ea5c483ddc6836a02
littlecapa <littlecapa@Rogers-Mac-min.fritz.box>
2022-04-16 14:38:27 +0200

    Working Login

--- chessCrawler/crawl/urls.py
+++ chessCrawler/crawl/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
   # /
   path('', views.home, name='home'),
   # TEMPORARY
-  path('signin', views.home, name='signin'),
+  path('signin', views.sign_in, name='signin'),
   path('signout', views.home, name='signout'),
   path('calendar', views.home, name='calendar'),
+  path('callback', views.callback, name='callback'),
+  path('signout', views.sign_out, name='signout'),
 ]
